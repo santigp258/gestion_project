4ab3bba465053ef4a0c198f3eb069a9936049445
daniel-villada <danielvillada980@gmail.com>
2020-12-26 16:46:10 -0500

    Separando estilos de login y registro

--- login/registerview.php
+++ login/registerview.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Register</title>
-  <link rel="stylesheet" href="../css/estilos.css" />
+  <link rel="stylesheet" href="../css/estilos-registro.css" />
   <!-- <link rel="stylesheet" href="../css/fontawesome-all.min.css" /> -->
   <link rel="icon" href="img/logo.ico" />
 </head>
