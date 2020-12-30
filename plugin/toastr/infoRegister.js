fe17fd01deda7890df0cac82587ef8354376b554
santigp258 <santigp258@gmail.com>
2020-12-30 17:52:01 -0500

    upgrade code

--- plugin/toastr/infoRegister.js
+++ plugin/toastr/infoRegister.js
@@ -2,18 +2,5 @@ toastr["success"]("¡Se ha completa su registro exitosamente!")
 
 toastr.options = {
   "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
   "progressBar": true,
-  "positionClass": "toast-top-right",
-  "preventDuplicates": false,
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
 }
