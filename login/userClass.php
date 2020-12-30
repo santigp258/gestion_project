63bb92b0be4ac0323445824d314767e51a7d061a
santigp258 <santigp258@gmail.com>
2020-12-29 21:56:32 -0500

    deleten  variable into UserClass->userRegistration

--- login/userClass.php
+++ login/userClass.php
@@ -46,7 +46,7 @@ class userClass
                 $stmt->execute();
                 $uid = $db->lastInsertId(); // Last inserted row id
                 $db = null;
-                $_SESSION['uid'] = $uid;
+              /*   $_SESSION['uid'] = $uid; */
                 return true;
             } else {
                 $db = null;
