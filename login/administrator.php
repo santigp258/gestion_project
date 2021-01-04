e34decbae31c6eedcfa6eebe0a079dd31d7bf215
santigp258 <santigp258@gmail.com>
2021-01-04 05:46:46 -0500

    administrator.php error solutioned

--- login/administrator.php
+++ login/administrator.php
@@ -35,7 +35,7 @@
 
 
   //pagination
- $uid = $session_uid ;
+  $uid = $session_uid;
   $db = getDB();
   $stmt = $db->prepare("SELECT * FROM afiliaciones WHERE id_users=:uid");
   $stmt->bindParam("uid", $uid, PDO::PARAM_INT);
@@ -45,23 +45,27 @@
   //count 
   $pagination = 4; //data for page
   $total_register_db = $stmt->rowCount();
-  $round = $total_register_db/$pagination; //calculate
+  $round = $total_register_db / $pagination; //calculate
   $pages = ceil($round); //round
 
-  //default page
-  if(!$_GET){
-    $url = BASE_URL . 'login/administrator.php?page=1';
-    header("Location: $url");
-  }
-  //redirect if pagination doesn't exists
-  if($_GET['page'] > $pages || $_GET['page'] < 1){
+  //default pag
+  if (!$_GET) {
     $url = BASE_URL . 'login/administrator.php?page=1';
     header("Location: $url");
   }
 
-  $index = ($_GET['page']-1)* $pagination;
+  /*  if(isset($_GET['page'])){
+    //redirect if pagination doesn't exists
+    if($_GET['page'] > $pages || $_GET['page'] < 1){
+      $url = BASE_URL . 'login/administrator.php?page=1';
+      header("Location: $url");
+    } 
+  }
+  */
 
-  $information = showInformation($session_uid, $index, $pagination); 
+  $index = ($_GET['page'] - 1) * $pagination;
+
+  $information = showInformation($session_uid, $index, $pagination);
   ?>
 
 
@@ -79,101 +83,105 @@
     </section>
 
     <!-- Comienzo de los Cruds -->
-    <section>
-      <div class="container my-3 ">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="card rouded-0 mt-4">
-              <!-- Buscar y agregar afiliacion -->
-              <div class="container m-2">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light-white">
-                  <form class="form-inline my-2 my-lg-0 lead" action="javascript: e.preventDefault()">
-                    <i class="icon ion-md-search" aria-hidden="true"></i>
-                    <input class="form-control mr-sm-2 " type="search" placeholder="Buscar" aria-label="Search" id="search">
-                  </form>
-                  <!-- Search form -->
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                      <a class="icon ion-md-add lead" href="create.php"><span class="p-2">Crear
-                          Afiliacion</span></a>
-                    </ul>
-                  </div>
-                </nav>
-              </div>
-              <!-- Afiliacion y buscar -->
+    <?php if ($stmt->rowCount() != 0) { ?>
+      <section>
+        <div class="container my-3 ">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="card rouded-0 mt-4">
+                <!-- Buscar y agregar afiliacion -->
+                <div class="container m-2">
+                  <nav class="navbar navbar-expand-lg navbar-light bg-light-white">
+                    <form class="form-inline my-2 my-lg-0 lead" action="javascript: e.preventDefault()">
+                      <i class="icon ion-md-search" aria-hidden="true"></i>
+                      <input class="form-control mr-sm-2 " type="search" placeholder="Buscar" aria-label="Search" id="search">
+                    </form>
 
-              <!-- Inicio de Tabla -->
-              <div class="table-responsive">
-                <table class=" table table-hover">
-                  <thead>
-                    <tr>
-                      <th scope="col">ID</th>
-                      <th scope="col">Nombre</th>
-                      <th scope="col">Cédula</th>
-                      <th scope="col">Teléfono</th>
-                      <th scope="col">Ciudad</th>
-                      <th scope="col">Correo</th>
-                      <th scope="col">F. de Afiliación</th>
-                      <th scope="col">Acción</th>
-                    </tr>
-                  </thead>
-                  <tbody id="tbody">
-                     <?php
-                    foreach ($information as $info) { ?>
-                    <tr>
-                      <th scope="row"><?php echo $info->id ?></th>
-                      <td><?php echo $info->nombre ?></td>
-                      <td><?php echo $info->cedula ?></td>
-                      <td><?php echo $info->telefono ?></td>
-                      <td><?php echo $info->ciudad ?></td>
-                      <td><?php echo $info->email ?></td>
-                      <td><?php echo $info->f_afiliacion ?></td>
-                      <td>
-                        <a href="show.php"><span class='icon ion-md-eye lead' style="color:var(--primary)"></span></a>
-                        <a href="edit.php"><span class='icon ion-md-create lead' style="color:var(--orange)"></span></a>
-                        <a href="#"><span class='icon ion-md-trash lead' style="color:var(--red);"></span></a>
-                      </td>
-                    </tr>
-                    <?php
-                    } ?>  
-                  </tbody>
-                  <input type="hidden" value="<?php echo $index ?>" id="index">
-                </table>
-              </div>
-              <!-- Pagination -->
-              <div class="pagination p-0 m-auto" id="div_pagination">
-                <ul class="pagination">
-                  <li class="page-item <?php echo $_GET['page'] <= 1 ? 'disabled' : '' ?>">
-                    <a class="page-link"
-                      href="<?php echo BASE_URL ?>login/administrator.php?page=<?php echo $_GET['page'] - 1 ?>"
-                      aria-label="Previous">
-                      <span aria-hidden="true">&laquo;</span>
-                      <span class="sr-only">Previous</span>
+                    <!-- Search form -->
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav ml-auto">
+                        <a class="icon ion-md-add lead" href="create.php"><span class="p-2">Crear
+                            Afiliacion</span></a>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+                <!-- Afiliacion y buscar -->
+
+                <!-- Inicio de Tabla -->
+                <div class="table-responsive">
+                  <table class=" table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Cédula</th>
+                        <th scope="col">Teléfono</th>
+                        <th scope="col">Ciudad</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">F. de Afiliación</th>
+                        <th scope="col">Acción</th>
+                      </tr>
+                    </thead>
+                    <tbody id="tbody">
+                      <?php
+                      foreach ($information as $info) { ?>
+                        <tr>
+                          <th scope="row"><?php echo $info->id ?></th>
+                          <td><?php echo $info->nombre ?></td>
+                          <td><?php echo $info->cedula ?></td>
+                          <td><?php echo $info->telefono ?></td>
+                          <td><?php echo $info->ciudad ?></td>
+                          <td><?php echo $info->email ?></td>
+                          <td><?php echo $info->f_afiliacion ?></td>
+                          <td>
+                            <a href="show.php"><span class='icon ion-md-eye lead' style="color:var(--primary)"></span></a>
+                            <a href="edit.php"><span class='icon ion-md-create lead' style="color:var(--orange)"></span></a>
+                            <a href="#"><span class='icon ion-md-trash lead' style="color:var(--red);"></span></a>
+                          </td>
+                        </tr>
+                      <?php
+                      } ?>
+                    </tbody>
+                    <input type="hidden" value="<?php echo $index ?>" id="index">
+                  </table>
+                </div>
+                <!-- Pagination -->
+                <div class="pagination p-0 m-auto" id="div_pagination">
+                  <ul class="pagination">
+                    <li class="page-item <?php echo $_GET['page'] <= 1 ? 'disabled' : '' ?>">
+                      <a class="page-link" href="<?php echo BASE_URL ?>login/administrator.php?page=<?php echo $_GET['page'] - 1 ?>" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                    </li>
+                    <?php for ($i = 0; $i < $pages; $i++) { ?>
+                      <li class="page-item
+                  <?php echo $_GET['page'] == $i + 1 ? 'active' : '' ?>"><a class="page-link" href="<?php echo BASE_URL ?>login/administrator.php?page=<?php echo $i + 1; ?>"><?php echo $i + 1; ?></a>
+                      </li>
+                    <?php } ?>
+                    <a class="page-link" <?php echo $_GET['page'] >= $pages ? 'disabled' : '' ?>href="<?php echo BASE_URL ?>login/administrator.php?page=<?php echo $_GET['page'] + 1 ?>" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                      <span class="sr-only ">Next</span>
                     </a>
-                  </li>
-                  <?php for ($i=0; $i < $pages; $i++){ ?>
-                  <li class="page-item
-                  <?php echo $_GET['page'] == $i + 1 ? 'active' : '' ?>"><a class="page-link"
-                      href="<?php echo BASE_URL ?>login/administrator.php?page=<?php echo $i + 1; ?>"><?php echo $i + 1; ?></a>
-                  </li>
-                  <?php }?>
-                  <a class="page-link"
-                    <?php echo $_GET['page'] >= $pages ? 'disabled' : '' ?>href="<?php echo BASE_URL ?>login/administrator.php?page=<?php echo $_GET['page'] + 1 ?>"
-                    aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                    <span class="sr-only ">Next</span>
-                  </a>
-                  </li>
+                    </li>
 
-                </ul>
-              </div>
+                  </ul>
+                </div>
+              <?php } else { ?>
+                <!-- Bienvenidad -->
+                <div class="bienvenidad">
+                  <img src="<?php echo BASE_URL ?>img/home.svg" alt="background">
+                </div> <!-- End Bienvenidad  -->
+
+              <?php } ?>
               <!-- Pagination end -->
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <!-- Fin de los Cruds -->
+      </section>
+      <!-- Fin de los Cruds -->
 
   </div> <!-- End Administrar Afiliaciones -->
 
