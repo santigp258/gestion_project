468694f04d90cc641991c52eacf0983e71f6c2db
santigp258 <santigp258@gmail.com>
2021-01-04 08:24:55 -0500

    footerscripts

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<!-- Scripts Bootstrap -->
+<script src="<?php echo BASE_URL ?>js/jquery.js">
+</script>
+<script src="<?php echo BASE_URL ?>js/CDN/popper.js">
+</script>
+<script src="<?php echo BASE_URL ?>js/CDN/bootstrap.js">
+</script>
+<!-- Scripts Bootstrap End -->
+
