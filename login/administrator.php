40151dfc5f4e92103cabcc6f598db5e880a646ba
santigp258 <santigp258@gmail.com>
2021-01-06 13:58:27 -0500

    administrator

--- login/administrator.php
+++ login/administrator.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?php echo BASE_URL ?>css/menu.css">
   <link rel="stylesheet" href="<?php echo BASE_URL ?>css/CDN/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo BASE_URL ?>css/dashboard.css">
   <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
